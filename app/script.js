efddbabddc4005f2294f10cbb9b9ed59af76b881
k03mad <k03.mad@gmail.com>
2020-03-08 04:58:55 +0300

    :full_moon_with_face: add some consts

--- app/script.js
+++ app/script.js
@@ -2,94 +2,100 @@
 
 const {green, blue, yellow, magenta, cyan} = require('colorette');
 const {hidemy: {code}} = require('../env');
-const {request, array, mikrotik, promise} = require('utils-mad');
+const {request, array, mikrotik, promise, print} = require('utils-mad');
+
+const API_URL = 'https://hidemy.name/api/pptp.php';
+
+const VPN_CONNECT_RETRIES = 5;
+
+const VPN_LIST_NEWLINE = /\r\n/;
+const VPN_LIST_SEPARATOR = / - |, /g;
+
+const chooseFromCountries = [
+    'Czech Republic',
+    'Estonia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Latvia',
+    'Lithuania',
+    'Poland',
+    'Romania',
+    'Serbia',
+    'Slovenia',
+];
 
 (async () => {
+    try {
 
-    const chooseFromCountries = [
-        'Czech Republic',
-        'Estonia',
-        'Finland',
-        'Germany',
-        'Hungary',
-        'Latvia',
-        'Lithuania',
-        'Poland',
-        'Romania',
-        'Serbia',
-        'Slovenia',
-    ];
+        const {body} = await request.got(API_URL, {searchParams: {serverlist: '', code}});
 
-    const {body} = await request.got('https://hidemy.name/api/pptp.php', {
-        searchParams: {serverlist: '', code},
-    });
+        const parsedList = body
+            .split(VPN_LIST_NEWLINE)
+            .filter(Boolean)
+            .map(entry => {
+                const [ip, country, city] = entry.split(VPN_LIST_SEPARATOR);
+                return {ip, country, city};
+            })
+            .sort((a, b) => {
+                if (a.country < b.country) {
+                    return -1;
+                }
 
-    const parsedList = body
-        .split(/\r\n/)
-        .filter(Boolean)
-        .map(entry => {
-            const [ip, country, city] = entry
-                .split(/ - |, /g)
-                .map(elem => elem.replace(/ [A-Z]\d+$/, ''));
+                if (a.country > b.country) {
+                    return 1;
+                }
 
-            return {ip, country, city};
-        })
-        .sort((a, b) => {
-            if (a.country < b.country) {
-                return -1;
+                return 0;
+            });
+
+        let country;
+        parsedList.forEach(elem => {
+            if (country !== elem.country) {
+                console.log(`\n${yellow(elem.country)}`);
             }
 
-            if (a.country > b.country) {
-                return 1;
-            }
-
-            return 0;
+            console.log(`${green(elem.city)}: ${blue(elem.ip)}`);
+            ({country} = elem);
         });
 
-    let country;
-    parsedList.forEach(elem => {
-        if (country !== elem.country) {
-            console.log(`\n${yellow(elem.country)}`);
-        }
+        const filteredCountries = parsedList.filter(elem => chooseFromCountries.includes(elem.country));
+        console.log(`\nParsed IPs: ${parsedList.length}`);
+        console.log(`Filtered by countries: ${filteredCountries.length}`);
 
-        console.log(`${green(elem.city)}: ${blue(elem.ip)}`);
-        ({country} = elem);
-    });
+        for (const choosenServer of array.shuffle(filteredCountries)) {
+            const comment = `${choosenServer.country}/${choosenServer.city}`;
 
-    const filteredCountries = parsedList.filter(elem => chooseFromCountries.includes(elem.country));
-    console.log(`\nParsed IPs: ${parsedList.length}`);
-    console.log(`Filtered by countries: ${filteredCountries.length}`);
+            const [before] = await mikrotik.write('/interface/pptp-client/print');
+            console.log(cyan(`\nCurrent PPTP Server: ${before.comment}`));
+            console.log(`${blue(before['connect-to'])}: running ${before.running}`);
 
-    for (const choosenServer of array.shuffle(filteredCountries).slice(0, 5)) {
-        const comment = `${choosenServer.country}/${choosenServer.city}`;
+            await mikrotik.write([
+                ['/interface/pptp-client/set', `=.id=${before['.id']}`, `=connect-to=${choosenServer.ip}`],
+                ['/interface/pptp-client/set', `=.id=${before['.id']}`, `=comment=${comment}`],
+            ]);
+            console.log(magenta(`\nNew PPTP Server: ${comment}`));
 
-        const [before] = await mikrotik.write('/interface/pptp-client/print');
-        console.log(cyan(`\nCurrent PPTP Server: ${before.comment}`));
-        console.log(`${blue(before['connect-to'])}: running ${before.running}`);
+            let found;
 
-        await mikrotik.write([
-            ['/interface/pptp-client/set', `=.id=${before['.id']}`, `=connect-to=${choosenServer.ip}`],
-            ['/interface/pptp-client/set', `=.id=${before['.id']}`, `=comment=${comment}`],
-        ]);
-        console.log(magenta(`\nNew PPTP Server: ${comment}`));
+            for (let i = 0; i < VPN_CONNECT_RETRIES; i++) {
+                await promise.delay();
 
-        let found;
+                const [after] = await mikrotik.write('/interface/pptp-client/print');
+                console.log(`${blue(after['connect-to'])}: running ${after.running}`);
 
-        for (let i = 0; i < 7; i++) {
-            await promise.delay();
+                if (after.running === 'true') {
+                    found = true;
+                    break;
+                }
+            }
 
-            const [after] = await mikrotik.write('/interface/pptp-client/print');
-            console.log(`${blue(after['connect-to'])}: running ${after.running}`);
-
-            if (after.running === 'true') {
-                found = true;
+            if (found) {
                 break;
             }
         }
 
-        if (found) {
-            break;
-        }
+    } catch (err) {
+        print(err, {full: true, exit: true});
     }
-
 })();
